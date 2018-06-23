b04330d3cc5e4bb12350f31bac6d1b3dbcbd1bea
thomasmargand <thomas.c.margand@outlook.com>
2018-06-23 15:24:46 -0400

    update

---
+++ Exercises/Exercise12/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number between 10 - 1000");
+            int n = int.Parse(Console.ReadLine());
+            List<int> Zeroth = RandomList(n);
+            foreach (int num in Zeroth)
+            {
+                Console.Write($"{num}, ");
+            }
+            Console.WriteLine();
+
+            int avg = Mean(Zeroth);
+            Console.WriteLine($"The mean of the list is {avg}");
+
+            double med = Median(Zeroth);
+            Console.WriteLine($"The median of the list is {med}");
+
+            int sd = StandardDeviation(Zeroth);
+            Console.WriteLine($"The standard deviation is {sd}");
+
+            double mAD = MedianAD(Zeroth);
+            Console.WriteLine($"The median absolute deviation is {mAD}");
+
+            double skew = Skewness(Zeroth);
+            Console.WriteLine($"The skewness is {skew}");
+
+            double kurt = Kurtosis(Zeroth);
+            Console.WriteLine($"The kurtosis is {kurt}");            
+        }
+
+        public static List<int> RandomList(int n)
+        {
+            List<int> result = new List<int>();
+            for (int i = 0; i < n; i++)
+            {
+                result.Add(new Random(i).Next(1000));
+            }
+            return result;
+        }
+
+        public static int Mean(List<int> intList)
+        {
+            return (int)intList.Average();
+        }
+
+        public static double Median(List<int> intList)
+        {
+            intList.Sort();
+            if (intList.Count % 2 != 0)
+                return (intList[intList.Count / 2 + 1]);
+
+            else
+            {
+                double half1 = intList[intList.Count / 2];
+                double half2 = intList[intList.Count / 2 + 1];
+                return (half1 + half2) / 2;
+            }
+        }
+        public static int StandardDeviation(List<int> intList)
+        {
+            List<int> numerator = new List<int>();
+            double median = Median(intList);
+            double average = Mean(intList);
+
+            foreach (int num in intList)
+            {
+                int temp = (int)(Math.Pow(num - average, 2));
+                numerator.Add(temp);
+            }
+            int total = numerator.Sum();
+            return (int)Math.Sqrt(total / intList.Count);
+        }
+        public static double MedianAD(List<int> intList)
+        {
+            intList.Sort();
+            double median = Median(intList);
+            List<int> absList = new List<int>();
+
+            foreach (int num in intList)
+            {
+                absList.Add((int)Math.Abs(num - median));
+            }
+            return Median(absList);
+        }
+        public static double Skewness(List<int> intList)
+        {
+            return (3 * (Mean(intList) - Median(intList)) / StandardDeviation(intList));
+        }
+        public static double Kurtosis(List<int> intList)
+        {
+            List<double> numerator = new List<double>();
+            foreach (int num in intList)
+            {
+                numerator.Add(Math.Pow((num - Mean(intList)), 4));
+            }
+            double total = numerator.Sum();
+            double lastNumerator = total / intList.Count;
+            double denominator = Math.Pow(StandardDeviation(intList), 4);
+            return lastNumerator / denominator;
+        }
+    }    
+}
---
+++ Exercises/Exercise13/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter plain text: ");
+            string plain_text = Console.ReadLine();
+            Console.WriteLine($"You entered [{plain_text}]");
+
+            Console.WriteLine("Enter your key as an upper case alpha character: ");
+            string key = Console.ReadLine();
+            Console.WriteLine($"You entered [{key}]");
+
+            int[] clean_text = Clean(plain_text);
+            int enc_length = 0;
+            for (int i = 0; i < clean_text.Length; i++)
+            {
+                Console.Write($"[{clean_text[i]}]");
+                if (clean_text[i] >= 65 || clean_text[i] <= 90) enc_length++;
+            }
+            Console.WriteLine();
+
+            int[] clean_key = Clean(key);
+            for (int i = 0; i < clean_key.Length; i++)
+                Console.Write($"[{clean_key[i]}]");
+            Console.WriteLine();
+
+            string encrypted = Encrypted(clean_text, clean_key, enc_length);
+            Console.WriteLine($"{encrypted}");
+          
+        }
+
+        private static string Encrypted(int[] clean_text, int[] clean_key, int len)
+        {
+            //int len = clean_text.Length;
+            int[] encrypted = new int[len];
+            for (int i = 0; i < len; i++)
+            {
+                encrypted[i] = (clean_text[i] + clean_key[0] - 64);
+                if (encrypted[i] > 90) encrypted[i] -= +26;
+                Console.Write($"[{encrypted[i]}]");
+            }
+            Console.WriteLine();
+            char[] char_result = new char[len];
+            for(int i = 0; i < len; i++)
+                char_result[i] = (char)encrypted[i];
+            string result = new string(char_result);
+            return result;
+        }
+
+        private static int[] Clean(string input)
+        {
+            int[] result = new int[input.Length];
+            for (int i = 0, j = -1; i < input.Length; i++)
+            {
+                int c = (int)input[i];
+                if (c > 90) c -= 32;
+                if (c < 65 || c > 90) continue;
+                result[++j] = c;
+                Console.WriteLine($"{c} == {input[i]} == " +
+                    $"{(char)c} == {result[j]}");
+
+            }
+            return result;
+        }
+    }
+}
